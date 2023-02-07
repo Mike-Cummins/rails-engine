a8a92ca9965025534fdc7de6a8dee094779ae976
Mike Cummins <Cummins.Michael.R@gmail.com>
2023-02-07 12:28:29 -0800

    Refactor test to pass, Postman test passing

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -8,15 +8,19 @@ describe 'Merchants API' do
     expect(response).to be_successful
 
     merchants = JSON.parse(response.body, symbolize_names: true)
-
-    expect(merchants.count).to eq(3)
-
-    merchants.each do |merchant|
+  
+    expect(merchants[:data].count).to eq(3)
+    
+    merchants[:data].each do |merchant|
       expect(merchant).to have_key(:id)
-      expect(merchant[:id]).to be_an(Integer)
+      expect(merchant[:id]).to be_an(String)
 
-      expect(merchant).to have_key(:name)
-      expect(merchant[:name]).to be_a(String)
+      expect(merchant).to have_key(:type)
+      expect(merchant[:type]).to be_a(String)
+
+      expect(merchant).to have_key(:attributes)
+      expect(merchant[:attributes]).to be_a(Hash)
+      expect(merchant[:attributes][:name]).to be_a(String)
     end
   end
 end
