f42c8aab0bd42e21cba168a3a379a5ba5c259055
Mike Cummins <Cummins.Michael.R@gmail.com>
2023-02-08 12:55:31 -0800

    Add merchant by item, test, Postman test passing

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
       resources :merchants, only: [:index, :show] do
         resources :items, only: [:index], controller: 'merchants/items'
       end
-      resources :items, only: [:index, :show]
+      resources :items, only: [:index, :show] 
+      get '/items/:id/merchant', to: 'items/merchant#show'
+     
     end
   end
 end
