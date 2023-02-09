19a1efcbb02cdbbe2128c964d2c6a77f398ff766
Mike Cummins <Cummins.Michael.R@gmail.com>
2023-02-08 18:23:48 -0800

    Add item create, test, Postman test passing

--- spec/requests/api/v1/items/items_request_spec.rb
+++ spec/requests/api/v1/items/items_request_spec.rb
@@ -70,4 +70,51 @@ describe 'Items API' do
     expect(item[:data][:attributes]).to have_key(:merchant_id)
     expect(item[:data][:attributes][:merchant_id]).to be_a(Integer)
   end
+
+  it 'Can create a new item' do
+    create_list(:item, 3)
+    merchant = create(:merchant)
+    item_data = { 
+                  name: 'Some item',
+                  description: 'Stuff about some item',
+                  unit_price: 99.99,
+                  merchant_id: merchant.id 
+                }
+
+    headers = { 'CONTENT_TYPE' => 'application/json' }
+
+    expect(Item.all.count).to eq(3)
+
+    post '/api/v1/items', headers: headers, params: JSON.generate(item: item_data)
+
+    expect(response).to be_successful
+
+    item = JSON.parse(response.body, symbolize_names: true)
+    
+    expect(Item.all.count).to eq(4)
+
+    expect(item).to have_key(:data)
+    expect(item[:data]).to be_a(Hash)
+
+    expect(item[:data]).to have_key(:id)
+    expect(item[:data][:id]).to be_a(String)
+
+    expect(item[:data]).to have_key(:type)
+    expect(item[:data][:type]).to be_a(String)
+
+    expect(item[:data]).to have_key(:attributes)
+    expect(item[:data][:attributes]).to be_a(Hash)
+
+    expect(item[:data][:attributes]).to have_key(:name)
+    expect(item[:data][:attributes][:name]).to be_a(String)
+
+    expect(item[:data][:attributes]).to have_key(:description)
+    expect(item[:data][:attributes][:description]).to be_a(String)
+
+    expect(item[:data][:attributes]).to have_key(:unit_price)
+    expect(item[:data][:attributes][:unit_price]).to be_a(Float)
+
+    expect(item[:data][:attributes]).to have_key(:merchant_id)
+    expect(item[:data][:attributes][:merchant_id]).to be_a(Integer)
+  end
 end
