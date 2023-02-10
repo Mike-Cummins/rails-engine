cf3dacbd0e515190356e96ec29ad65ecccfa5660
Mike Cummins <Cummins.Michael.R@gmail.com>
2023-02-09 16:27:48 -0800

    Add test and conditional to find with only max price

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,7 @@ class Item < ApplicationRecord
   belongs_to :merchant
 
   def self.find_all_by_price(min, max)
-    where('unit_price >= ?', min).where('unit_price <= ?', max).order(Arel.sql('lower(name)'))
+    min = 0 if min.nil?
+    Item.where('unit_price >= ?', min).where('unit_price <= ?', max).order(Arel.sql('lower(name)'))
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,12 +4,19 @@ RSpec.describe Item, type: :model do
   describe 'Relationships' do
     it {should belong_to :merchant}
   end
-
-  it 'Finds all items by price search' do
-    item_1 = create(:item, unit_price: 12.99)
-    item_2 = create(:item, unit_price: 9.99)
-    item_3 = create(:item, unit_price: 19.99)
+  before :each do
+    @item_1 = create(:item, unit_price: 12.99)
+    @item_2 = create(:item, unit_price: 9.99)
+    @item_3 = create(:item, unit_price: 19.99)
     
-    expect(Item.find_all_by_price(10, 20)).to eq([item_1, item_3])
+  end
+  it 'Finds all items by min and max price search' do
+
+    expect(Item.find_all_by_price(10, 20)).to eq([@item_1, @item_3])
+  end
+
+  it 'Finds all items by max price only search' do
+
+    expect(Item.find_all_by_price(nil, 20)).to eq([@item_1, @item_2, @item_3])
   end
 end
