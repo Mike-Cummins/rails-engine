6dd2c83254c8372599da4702a0339e9767b21db7
Mike Cummins <Cummins.Michael.R@gmail.com>
2023-02-09 16:17:24 -0800

    Add test and method to find all items by min and max price

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :merchant
+
+  def self.find_all_by_price(min, max)
+    where('unit_price >= ?', min).where('unit_price <= ?', max).order(Arel.sql('lower(name)'))
+  end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe Item, type: :model do
   describe 'Relationships' do
     it {should belong_to :merchant}
   end
+
+  it 'Finds all items by price search' do
+    item_1 = create(:item, unit_price: 12.99)
+    item_2 = create(:item, unit_price: 9.99)
+    item_3 = create(:item, unit_price: 19.99)
+    
+    expect(Item.find_all_by_price(10, 20)).to eq([item_1, item_3])
+  end
 end
